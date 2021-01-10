4121d95e9c9d4a1ea4bb85d98547b81f0effa28c
Rui-FMF <ruifmf2000@gmail.com>
2021-01-10 16:47:01 +0000

    article-service

--- frontend/src/app/core/services/article.service.ts
+++ frontend/src/app/core/services/article.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
-import { ArticlesModule } from '../../features/articles/articles.module';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import {ArticleModel} from '@models/article.model';
 
-@Injectable({
-  providedIn: ArticlesModule
-})
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
+
+@Injectable()
 export class ArticleService {
+  private baseURL = 'http://localhost:8000/ws/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getArticles(): Observable<ArticleModel[]> {
+    const url = this.baseURL + 'articles';
+    return this.http.get<ArticleModel[]>(url, httpOptions);
+  }
+
+  getNArticles(num: number): Observable<ArticleModel[]> {
+    const url = this.baseURL + 'articles?num=' + num;
+    return this.http.get<ArticleModel[]>(url, httpOptions);
+  }
+
+  getArticle(id: number): Observable<ArticleModel> {
+    const url = this.baseURL + 'articles?id=' + id;
+    return this.http.get<ArticleModel>(url, httpOptions);
+  }
+
+  createArticle(art: ArticleModel): Observable<any> {
+    const url = this.baseURL + 'create/article/';
+    return this.http.post(url, art, httpOptions);
+  }
+
+  updateArticle(art: ArticleModel): Observable<any> {
+    const url = this.baseURL + 'update/article/';
+    return this.http.put(url, art, httpOptions);
+  }
+
+  deleteArticle(art: ArticleModel): Observable<any> {
+    const url = this.baseURL + 'delete/article/' + art.id + '/';
+    return this.http.delete<ArticleModel>(url, httpOptions);
+  }
 }
