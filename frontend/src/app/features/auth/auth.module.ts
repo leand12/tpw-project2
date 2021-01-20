411c623d70baf612acf08d334638b01253f9ba3e
Leandro Silva <leandrosilva12@ua.pt>
2021-01-20 02:54:57 +0000

    showing popular tags

--- frontend/src/app/features/auth/auth.module.ts
+++ frontend/src/app/features/auth/auth.module.ts
@@ -6,12 +6,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import {ArticlesModule} from '../articles/articles.module';
 
 
 @NgModule({
   imports: [
     SharedModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ArticlesModule
   ],
   declarations: [
     SignupComponent,
--- frontend/src/app/features/auth/home/home.component.ts
+++ frontend/src/app/features/auth/home/home.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
-import {ArticleModel} from '@core/models/article.model';
+import {ArticleService} from '@core/services/article.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ArticleService]
 })
 export class HomeComponent implements OnInit {
-  articles: any;
-  selectedPlatform: string;
+  popularArticles: any[];
 
-  constructor() { }
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit(): void {
-    this.selectedPlatform = 'PS';
-    this.articles = [
-      {id: 1, name: 'HARDCODED', total_price: '20.00', description: '', shipping_fee: '0.00', date_posted: '2021-01-07', tag: [9, 11],
-        is_sold: false, times_viewed: 0, shop_cart: [], saved: [], seller: 1, buyer: null},
-      {id: 2, name: 'HARDCODED2', total_price: '15.00', description: '', shipping_fee: '0.00', date_posted: '2021-01-07', tag: [9],
-        is_sold: false, times_viewed: 0, shop_cart: [], saved: [], seller: 1, buyer: null},
-      {id: 3, name: 'HARDCODED3', total_price: '10.00', description: '', shipping_fee: '0.00', date_posted: '2021-01-07', tag: [11],
-        is_sold: false, times_viewed: 0, shop_cart: [], saved: [], seller: 1, buyer: null},
-      {id: 4, name: 'HARDCODED4', total_price: '5.00', description: '', shipping_fee: '0.00', date_posted: '2021-01-07', tag: [],
-        is_sold: false, times_viewed: 0, shop_cart: [], saved: [], seller: 1, buyer: null}
-    ];
+    this.getPopularArticles();
   }
 
+  getPopularArticles(): void {
+    this.articleService.getArticlesFiltered(
+      6,
+      undefined,
+      undefined,
+      false,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      'true',
+    ).subscribe(
+      articles => this.popularArticles = articles,
+    );
+  }
 }
